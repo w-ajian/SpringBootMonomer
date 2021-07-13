1d0dc516b05a13f1fcc75556ca2e390035449d19
Aj <wenjianhaode@gmail.com>
2021-07-13 23:32:44 +0800

    2021-7-13晚间提交

--- src/test/java/com/wenjian/monomer/MybatisApplicationTests.java
+++ src/test/java/com/wenjian/monomer/MybatisApplicationTests.java
@@ -3,11 +3,22 @@ package com.wenjian.monomer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+
 @SpringBootTest
 class MybatisApplicationTests {
 
     @Test
     void contextLoads() {
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            System.out.print(i);
+        }
+        long end = new Date().getTime();
+        System.out.println();
+        System.out.println("共耗时毫秒：" + (end - start));
+
     }
 
 }
